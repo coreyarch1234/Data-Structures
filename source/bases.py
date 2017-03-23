c6cf7ba0e44f93a5d0410a4fb804714a29425654
coreyarch1234 <harrilal@mit.edu>
2017-03-23 11:31:43 -0700

    refacted neg bases

--- source/bases.py
+++ source/bases.py
@@ -10,14 +10,15 @@ def decode(str_num, base):
     str_num -- string representation of number in given base
     base -- base of given number
     """
+    assert 2 <= base <= 36
     universal_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
     'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] #create list of characters that can be used
     universal_dict = {}
     count = 0
-    for char in universal_list:
+    for char in universal_list: #assign values to keys in dictionary
         universal_dict[char] = count
         count += 1
-    num_by_base = reversed(list(str_num))
+    num_by_base = reversed(list(str_num)) #reverse list to start from right to left
     converted_val = 0
     convert_count = 0
     for num in num_by_base:
@@ -33,6 +34,7 @@ def encode(num, base):
     num -- the number in base 10
     base -- base to convert to
     """
+    assert 2 <= base <= 36
     universal_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
     'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] #create list of characters that can be used
     universal_dict = {}
@@ -43,12 +45,20 @@ def encode(num, base):
     reverse_universal_dict = dict((v,k) for k,v in universal_dict.iteritems())
     converted_val = ''
     remainder_val = 0
-
     while num != 0:
         remainder_val = int(num) % int(base) #remainder to be stored
         num = int(num) / int(base) #next number
-        converted_val += reverse_universal_dict[remainder_val]
-    return converted_val[::-1]
+        converted_val += reverse_universal_dict[remainder_val] #append remainder  to string
+    return converted_val[::-1] #return reversed string
+
+def convert(str_num, base1, base2):
+    """
+    Convert given number from base1 to base2.
+    """
+
+    base_ten = decode(str_num, base1) #change base1 number to base 10
+    converted_base = encode(base_ten, base2) #change base10 number to base2
+    return converted_base
 
 
 
@@ -65,7 +75,7 @@ def switch_num(str_num):
             # print start_index
             break
 
-    for updated_index in range(start_index + 1, len(str_num)):
+    for updated_index in range(start_index + 1, len(str_num)): #switch from 0 to 1 to find two's complement
         if str_num[updated_index] == "1":
             # print "string = 1"
             # print str_num
@@ -80,14 +90,9 @@ def switch_num(str_num):
 
     return "1 1 1 1 " + " ".join(reversed(str_num))
 
-
-
-
-
-
-def convert(str_num, base1, base2):
+def neg_convert(str_num, base1, base2): #Negative binary conversion
     """
-    Convert given number from base1 to base2.
+    Convert given base 10 whether negative or positive to it's two's complemen tnumber from base1 to base2.
     """
     if int(str_num) == 0:
         return "0000"
@@ -104,6 +109,8 @@ def convert(str_num, base1, base2):
 
 
 
+
+
 def main():
     import sys
     args = sys.argv[1:]  # Ignore script file name
@@ -111,7 +118,7 @@ def main():
         str_num = args[0]
         base1 = int(args[1])
         base2 = int(args[2])
-        result = convert(str_num, base1, base2)
+        result = neg_convert(str_num, base1, base2)
         print('{} in base {} is {} in base {}'.format(str_num, base1, result, base2))
     else:
         # print('Usage: {} number base1 base2'.format(sys.argv[0]))
