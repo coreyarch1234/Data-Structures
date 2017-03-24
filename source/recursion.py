9b0ebea79ed359edfc6677739bed3c8fa699087b
coreyarch1234 <harrilal@mit.edu>
2017-03-24 10:53:09 -0700

    finished factorial recursion

--- source/recursion.py
+++ source/recursion.py
@@ -8,16 +8,21 @@ def factorial(n):
     otherwise raises ValueError for n < 0 or non-integer n"""
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
+    # return factorial_recursive(n)
 
 
 def factorial_iterative(n):
-    # TODO: implement the factorial function iteratively here
-    pass
+    # # TODO: implement the factorial function iteratively here
+    # pass
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests below
-
+    factorial_answer = 1
+    if n < 0 or not isinstance(n, int):
+        raise ValueError('factorial is undefined for n = {}'.format(n))
+    for num in range(1, n + 1):
+        factorial_answer *= num
+    return factorial_answer
 
 def factorial_recursive(n):
     # check if n is negative or not an integer (invalid input)
--- source/search.py
+++ source/search.py
@@ -17,11 +17,16 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
+    # # TODO: implement linear search recursively here
+    # pass
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests below
-
+    if index + 1 == len(array):
+        return None
+    if item == array[index]:
+        return index
+    else:
+        return linear_search_recursive(array, item, index + 1)
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
