28ca34d67c350a6bc24331fa2a2d57d1e6900dab
coreyarch1234 <harrilal@mit.edu>
2017-04-07 13:07:34 -0700

    refactored

--- source/hashset.py
+++ source/hashset.py
@@ -7,7 +7,7 @@ class HashSet(object):
 
     def __init__(self, elements=None):
         """Initialize this hash table with the given initial size"""
-        # init_size=8
+        # init_size=8 implement hash table instance
         self.buckets = [LinkedList() for i in range(8)]
         self.size = 0  # Count number of key-value entries
         if elements:
@@ -67,7 +67,7 @@ class HashSet(object):
         return entry is not None  # True or False
 
     def add(self, element): #add key of set element and value of unit type - 1
-        """Insert or update the given key with its associated value"""
+        """Insert or update the given key with its associated value""" #with hash instance
         unit_type = 1 #dummy value
         # Find the bucket the given key belongs in
         index = self._bucket_index(element)
@@ -96,7 +96,7 @@ class HashSet(object):
             bucket.delete(entry)
             self.size -= 1
         else:  # Not found
-            raise KeyError('Element not found: {}'.format(element))
+            raise ValueError('Element not found: {}'.format(element))
 
     def union(self, second_set): #Assume the second set is of type, HashSet
         """Return the union of the current set and the second set, don't update this set"""
@@ -126,13 +126,11 @@ class HashSet(object):
 
     def is_subset(self, second_set): #Assume the second set is of type, HashSet
         """Return True if second_set if a subset of current set. False otherwise."""
-        isSubset = False
         for element in second_set.keys():
-            if element in self.keys(): #if they have the same element
-                continue
-            else:
-                return isSubset
+            if element not in self.keys(): #if they have the same element
+                return False
         return True
+
 
     def keys(self):
         """Return a list of all keys in this hash table"""
@@ -143,14 +141,6 @@ class HashSet(object):
                 all_keys.append(key)
         return all_keys
 
-    # def values(self):
-    #     """Return a list of all values in this hash table"""
-    #     # Collect all values in each of the buckets
-    #     all_values = []
-    #     for bucket in self.buckets:
-    #         for key, value in bucket.items():
-    #             all_values.append(value)
-    #     return all_values
 
     def items(self):
         """Return a list of all entries (key-value pairs) in this hash table"""
@@ -160,94 +150,11 @@ class HashSet(object):
             all_items.extend(bucket.items())
         return all_items
 
-    def length(self):
-        """Return the number of key-value entries by traversing its buckets"""
-        # Count number of key-value entries in each of the buckets
-        item_count = 0
-        for bucket in self.buckets:
-            item_count += bucket.length()
-        return item_count
-        # Equivalent to this list comprehension:
-        return sum(bucket.length() for bucket in self.buckets)
-
-    def get(self, key):
-        """Return the value associated with the given key, or raise KeyError"""
-        # Find the bucket the given key belongs in
-        index = self._bucket_index(key)
-        bucket = self.buckets[index]
-        # Find the entry with the given key in that bucket, if one exists
-        entry = bucket.find(lambda (k, v): k == key)
-        if entry is not None:  # Found
-            # Return the given key's associated value
-            assert isinstance(entry, tuple)
-            assert len(entry) == 2
-            return entry[1]
-        else:  # Not found
-            raise KeyError('Key not found: {}'.format(key))
-
 
 def test_hash_table():
     h_one = HashSet(["A", "B", "C", "1"])
-    h_two = HashSet(["A", "2"])
+    h_two = HashSet(["A", "B"])
     print(h_one.is_subset(h_two))
-    # h_one.add("A")
-    # h_one.add("B")
-    # h_two.add("C")
-    # h_difference = h_one.difference(h_two)
-    # print(h_difference)
-    # print(h_one)
-    # h_union = h_one.union(h_two)
-    # print(h_one)
-    # print(h_two)
-    # print(h_union)
-    # h_two.add("A")
-    # h_intersection = h_one.intersection(h_two)
-    # print(h_intersection)
-
-    # ht = HashTable(4)
-    # print('HashTable: ' + str(ht))
-    #
-    # print('Setting entries:')
-    # ht.set('I', 1)
-    # print('set(I, 1): ' + str(ht))
-    # ht.set('V', 5)
-    # print('set(V, 5): ' + str(ht))
-    # print('size: ' + str(ht.size))
-    # print('length: ' + str(ht.length()))
-    # print('buckets: ' + str(len(ht.buckets)))
-    # print('load_factor: ' + str(ht.load_factor()))
-    # ht.set('X', 10)
-    # print('set(X, 10): ' + str(ht))
-    # ht.set('L', 50)  # Should trigger resize
-    # print('set(L, 50): ' + str(ht))
-    # print('size: ' + str(ht.size))
-    # print('length: ' + str(ht.length()))
-    # print('buckets: ' + str(len(ht.buckets)))
-    # print('load_factor: ' + str(ht.load_factor()))
-    #
-    # print('Getting entries:')
-    # print('get(I): ' + str(ht.get('I')))
-    # print('get(V): ' + str(ht.get('V')))
-    # print('get(X): ' + str(ht.get('X')))
-    # print('get(L): ' + str(ht.get('L')))
-    # print('contains(X): ' + str(ht.contains('X')))
-    # print('contains(Z): ' + str(ht.contains('Z')))
-    #
-    # print('Deleting entries:')
-    # ht.delete('I')
-    # print('delete(I): ' + str(ht))
-    # ht.delete('V')
-    # print('delete(V): ' + str(ht))
-    # ht.delete('X')
-    # print('delete(X): ' + str(ht))
-    # ht.delete('L')
-    # print('delete(L): ' + str(ht))
-    # print('contains(X): ' + str(ht.contains('X')))
-    # print('size: ' + str(ht.size))
-    # print('length: ' + str(ht.length()))
-    # print('buckets: ' + str(len(ht.buckets)))
-    # print('load_factor: ' + str(ht.load_factor()))
-
 
 if __name__ == '__main__':
     test_hash_table()
